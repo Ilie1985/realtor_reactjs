1041d4faed218b40f24e0b2b1293d556bc7a1a64
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-27 17:05:55 +0000

    create spinner component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import OAuth from "./OAuth";
 import PrivateRoute from "./PrivateRoute";
+import Spinner from "./Spinner";
 
-export{Header,OAuth,PrivateRoute}
+export { Header, OAuth, PrivateRoute, Spinner };
