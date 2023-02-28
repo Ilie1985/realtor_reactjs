5d2d62e4d19e3e729eeedb9bc6881c716761be8c
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-28 12:31:49 +0000

    regular price input UI in create listing page

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -11,6 +11,7 @@ const CreateListing = () => {
     address: "",
     description: "",
     offer: false,
+    regularPrice: 0,
   });
   const {
     type,
@@ -22,6 +23,7 @@ const CreateListing = () => {
     address,
     description,
     offer,
+    regularPrice,
   } = formData;
   const onChange = () => {};
 
@@ -205,6 +207,28 @@ const CreateListing = () => {
           >
             No
           </button>
+        </div>
+        <div className="flex items-center mb-6">
+          <div>
+            <p className="text-lg font-semibold">Regular price</p>
+            <div className="flex w-full justify-center items-center space-x-6">
+              <input
+                type="number"
+                id="regularPrice"
+                value={regularPrice}
+                onChange={onChange}
+                min="50"
+                max="400000000"
+                required
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+              />
+              {type === "rent" && (
+                <div>
+                  <p className="text-md w-full whitespace-nowrap ">£ / Month</p>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </form>
     </main>
