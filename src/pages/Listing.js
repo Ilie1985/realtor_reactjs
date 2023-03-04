0fef7825f6065d9ce8d68b055e6575a306e00934
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-04 11:51:12 +0000

    contact component

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -20,6 +20,7 @@ import {
   FaChair,
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
 
 const Listing = () => {
   const auth = getAuth();
@@ -91,7 +92,7 @@ const Listing = () => {
 
       {/* LISTING INFORMATION */}
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className=" w-full h-[200px] lg-[400px]">
+        <div className=" w-full ">
           <p className="text-xl font-bold mb-3 text-blue-900 ">
             {listing.name} - £
             {listing.offer
@@ -125,7 +126,7 @@ const Listing = () => {
             {listing.description}
           </p>
 
-          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
             <li className="flex items-center whitespace-nowrap">
               <FaBed className="text-lg mr-1" />
               {+listing.bedrooms > 1
@@ -153,14 +154,19 @@ const Listing = () => {
           {listing.uerRef !== auth.currentUser?.uid && !contactLandlord && (
             <div className="mt-6 ">
               <button
+                type="button"
                 onClick={() => {
                   return setContactLandlord(true);
                 }}
-                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out"
+                className="px-7 py-3 bg-blue-600 text-white font-semibold text-sm uppercase rounded shadow-lg  w-full hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  transition duration-150 ease-in-out"
               >
                 Contact Landlord
               </button>
             </div>
+          )}
+
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
         <div className=" w-full h-[200px] lg-[400px]z-10 overflow-x-hidden"></div>
