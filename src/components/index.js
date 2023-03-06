98b5a043303b7c4b6dc8df48e57acdb70167ea3d
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-06 10:02:17 +0000

    map through imgUrls and display only the first img

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import OAuth from "./OAuth";
 import PrivateRoute from "./PrivateRoute";
 import Spinner from "./Spinner";
 import ListingItem from "./ListingItem";
+import Slider from "./Slider";
 
-export { Header, OAuth, PrivateRoute, Spinner,ListingItem};
+export { Header, OAuth, PrivateRoute, Spinner, ListingItem, Slider };
