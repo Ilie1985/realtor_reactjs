de19c0e8e5554905ce0440471a28bb76f370a7c4
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-06 12:23:07 +0000

    places for sale in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -75,6 +75,37 @@ const Home = () => {
     fetchListings();
   }, []);
 
+  //places for sale
+  const [saleListings, setSaleListings] = useState(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //get reference
+        const listingRef = collection(db, "listings");
+
+        //create query (the limit/condition of that request)
+        const q = query(
+          listingRef,
+          where("type", "==", "sale"),
+          orderBy("timestamp", "desc"),
+          limit(4)
+        );
+
+        //execute the query
+        const querySnap = await getDocs(q);
+        let listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({ id: doc.id, data: doc.data() });
+        });
+        setSaleListings(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchListings();
+  }, []);
+
   return (
     <div>
       <Slider />
@@ -116,6 +147,32 @@ const Home = () => {
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {rentListings.map((listing) => {
+                return (
+                  <ListingItem
+                    key={listing.id}
+                    listing={listing.data}
+                    id={listing.id}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+        )}
+
+        {/* places for sale */}
+        {saleListings && saleListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-2 font-semibold">
+              Places for Sale
+            </h2>
+
+            <Link to="/category/sale">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for sale
+              </p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {saleListings.map((listing) => {
                 return (
                   <ListingItem
                     key={listing.id}
